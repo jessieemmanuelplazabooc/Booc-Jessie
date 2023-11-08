38b8659516052ce5da4779e3eef1a0bbe1ecb8e2
jessieemmanuelplazabooc <148731911+jessieemmanuelplazabooc@users.noreply.github.com>
2023-11-07 17:19:09 -0800

    Add files via upload

---
+++ PRELIM/FavoriteSongs.java
@@ -0,0 +1,11 @@
+public class FavoriteSongs {
+    public static void main(String[] args) {
+    
+        System.out.println("My Favorite Songs: \n");
+        
+        System.out.println("$uicideBoy$ – ...And To Those I Love, Thanks For Sticking Arounds");
+        System.out.println("Iya Terra - Don't Matta ");
+        System.out.println("Matisyahu - One Day ");
+        System.out.println("SICKO MODE - by Travis Scott");
+    } 
+}
